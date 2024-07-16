d8fa381d91ac33bf5e9c71dafde7f7f163badc24
David Martínez Bergantiño <dmartinezb09@educarex.es>
2024-07-16 21:43:38 +0200

    feat(FrontEnd): :sparkles: Cambios en la base de datos, header y mainpage

--- DataBase/fotoDB.sql
+++ DataBase/fotoDB.sql
@@ -2,28 +2,39 @@ DROP DATABASE IF EXISTS fotoDB;
 CREATE DATABASE fotoDB;
 USE fotoDB;
 
-CREATE TABLE Categorias (
+CREATE TABLE Animal (
     id INT AUTO_INCREMENT,
     nombre VARCHAR(255),
     PRIMARY KEY (id)
 );
 
-CREATE TABLE Subcategorias (
+CREATE TABLE Familia (
     id INT AUTO_INCREMENT,
     nombre VARCHAR(255),
-    categoria_id INT,
+    animal_id INT,
     PRIMARY KEY (id),
-    FOREIGN KEY (categoria_id) REFERENCES Categorias(id)
+    FOREIGN KEY (animal_id) REFERENCES Animal(id)
 );
 
+CREATE TABLE Especie (
+    id INT AUTO_INCREMENT,
+    nombre VARCHAR(255),
+    nombre_cientifico VARCHAR(255),
+    familia_id INT,
+    animal_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (familia_id) REFERENCES Familia(id)
+);
 CREATE TABLE Fotos (
     id INT AUTO_INCREMENT,
     nombre VARCHAR(255),
-    categoria_id INT,
-    subcategoria_id INT,
+    animal_id INT,
+    familia_id INT,
+    especie_id INT,
     fecha DATE,
     url VARCHAR(255),
     PRIMARY KEY (id),
-    FOREIGN KEY (categoria_id) REFERENCES Categorias(id),
-    FOREIGN KEY (subcategoria_id) REFERENCES Subcategorias(id)
+    FOREIGN KEY (animal_id) REFERENCES Animal(id),
+    FOREIGN KEY (familia_id) REFERENCES Familia(id),
+    FOREIGN KEY (especie_id) REFERENCES Especie(id)
 );
